59f23ccf86e1f628a8000f2ca77a4777dad64684
Jujstme <just.tribe@gmail.com>
2023-04-03 17:36:50 +0200

    Added support for more versions of the game

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 #![no_std]
+extern crate alloc;
+use alloc::string::String;
 use asr::{signature::Signature, timer, timer::TimerState, watcher::Watcher, Address, Process, time::Duration};
+
+#[cfg(target_arch = "wasm32")]
+#[global_allocator]
+static ALLOCATOR: lol_alloc::LockedAllocator<lol_alloc::FreeListAllocator> = lol_alloc::LockedAllocator::new(lol_alloc::FreeListAllocator::new());
 
 #[cfg(all(not(test), target_arch = "wasm32"))]
 #[panic_handler]
@@ -24,18 +30,20 @@ struct State {
 
 struct ProcessInfo {
     game: Process,
+    is_64_bit: bool,
     main_module_base: Address,
     main_module_size: u64,
     addresses: Option<MemoryPtr>,
 }
 
 struct Watchers {
-    state: Watcher<u8>,
+    state: Watcher<GameState>,
     levelid: Watcher<Acts>,
 }
 
 struct MemoryPtr {
     room_id: Address,
+    room_id_array: Address,
 }
 
 #[derive(asr::Settings)]
@@ -125,9 +133,11 @@ impl ProcessInfo {
         let game = proc?;
         let main_module_base = game.get_module_address(proc_name?).ok()?;
         let main_module_size = game.get_module_size(proc_name?).ok()?;
-   
+        let is_64_bit = ROOMID_SIG64.scan_process_range(&game, main_module_base, main_module_size).is_some();
+
         Some(Self {
             game,
+            is_64_bit,
             main_module_base,
             main_module_size,
             addresses: None,
@@ -136,22 +146,28 @@ impl ProcessInfo {
 
     fn look_for_addresses(&mut self) -> Option<MemoryPtr> {
         let room_id: Address;
+        let room_id_array: Address;
         let game = &self.game;
-        
-        const SIG32: Signature<11> = Signature::new("8B 0D ???????? 83 C4 04 3B 0D");
-        const SIG64: Signature<6> = Signature::new("4D 0F 45 F5 8B 0D");
 
-        if let Some(sigscan) = SIG32.scan_process_range(game, self.main_module_base, self.main_module_size) {
-            room_id = Address(game.read::<u32>(Address(sigscan.0 + 2)).ok()? as u64)
-        } else if let Some(sigscan) = SIG64.scan_process_range(&self.game, self.main_module_base, self.main_module_size) {
+        if self.is_64_bit {
+            let sigscan = ROOMID_SIG64.scan_process_range(&self.game, self.main_module_base, self.main_module_size)?;
             let ptr = sigscan.0 + 6;
-            room_id = Address(ptr + 0x4 + game.read::<u32>(Address(ptr)).ok()? as u64)
+            room_id = Address(ptr + 0x4 + game.read::<u32>(Address(ptr)).ok()? as u64);
+
+            let sigscan = ROOMARRAY_SIG64.scan_process_range(&self.game, self.main_module_base, self.main_module_size)?;
+            let ptr = sigscan.0 + 5;
+            room_id_array = Address(ptr + 0x4 + game.read::<u32>(Address(ptr)).ok()? as u64);
         } else {
-            return None
+            let sigscan = ROOMID_SIG32.scan_process_range(game, self.main_module_base, self.main_module_size)?;
+            room_id = Address(game.read::<u32>(Address(sigscan.0 + 2)).ok()? as u64);
+
+            let sigscan = ROOMARRAY_SIG32.scan_process_range(game, self.main_module_base, self.main_module_size)?;
+            room_id_array = Address(game.read::<u32>(Address(sigscan.0 + 2)).ok()? as u64);
         }
 
         Some(MemoryPtr {
-            room_id
+            room_id,
+            room_id_array,
         })
     }
 }
@@ -183,37 +199,82 @@ impl State {
         let Some(addresses) = &game.addresses else { return };
         let proc = &game.game;
 
-        let Some(state) = self.watchers.state.update(proc.read(addresses.room_id).ok()) else { return };
-
-        let act = match state.current {
-            70 | 71 | 72 => Acts::AngelIsland,
-            40 => Acts::ZoneZero,
-            41 => Acts::GreatTurquoise1,
-            42 => Acts::GreatTurquoise2,
-            43 => Acts::SunsetPark1,
-            44 => Acts::SunsetPark2,
-            45 => Acts::SunsetPark3,
-            46 => Acts::MetaJunglira1,
-            47 => Acts::MetaJunglira2,
-            48 => Acts::EggZeppelin,
-            49 => Acts::RobotnikWinter1,
-            50 | 51 => Acts::RobotnikWinter2,
-            52 => Acts::TidalPlant1,
-            53 => Acts::TidalPlant2,
-            54 => Acts::TidalPlant3,
-            55 => Acts::AtomicDestroyer1,
-            56 => Acts::AtomicDestroyer2,
-            57 => Acts::AtomicDestroyer3,
-            58 => Acts::FinalTrouble,
-            69 => Acts::PurplePalace,
-            0 | 1 | 2 | 9 => Acts::Credits,
-            _ => Acts::None,
-        };
-        if act != Acts::None {
-            self.watchers.levelid.update(Some(act));
-        } else if let Some(old_act) = &self.watchers.levelid.pair {
-            self.watchers.levelid.update(Some(old_act.current));
+        fn get_string(input: &[u8]) -> String {
+            let mut output = String::new();
+            for val in input{
+                let val = *val;
+                if val == 0 {
+                    break
+                } else {
+                    output.push(val as char)
+                }
+            }
+            output
         }
+
+        let room_id = match proc.read::<u32>(addresses.room_id) {
+            Ok(x) => x,
+            _ => 0,
+        };
+
+        let mut room_name = String::new();
+
+        if game.is_64_bit {
+            if let Ok(addr) = proc.read::<u64>(addresses.room_id_array) {
+                if let Ok(addr) = proc.read::<u64>(Address(addr + room_id as u64 * 8)) {
+                    if let Ok(addr) = proc.read::<[u8; 25]>(Address(addr)) {
+                        room_name = get_string(&addr);
+                    }
+                }
+            }
+        } else {
+            if let Ok(addr) = proc.read::<u32>(addresses.room_id_array) {
+                if let Ok(addr) = proc.read::<u32>(Address(addr as u64 + room_id as u64 * 4)) {
+                    if let Ok(addr) = proc.read::<[u8; 25]>(Address(addr as u64)) {
+                        room_name = get_string(&addr);
+                    }
+                }
+            }
+        }
+
+        let room_name_str = room_name.as_str();
+
+        let act = match room_name_str {
+            "rmAIZ" | "rmZONE0bit" | "rmZIBbit" => Acts::AngelIsland,
+            "rmZONE0" => Acts::ZoneZero,
+            "rmGTZ1" => Acts::GreatTurquoise1,
+            "rmGTZ2" => Acts::GreatTurquoise2,
+            "rmSPZ1" => Acts::SunsetPark1,
+            "rmSPZ2" => Acts::SunsetPark2,
+            "rmSPZ3" => Acts::SunsetPark3,
+            "rmMJZ1" => Acts::MetaJunglira1,
+            "rmMJZ2" => Acts::MetaJunglira2,
+            "rmEZZ" => Acts::EggZeppelin,
+            "rmRWZ1" => Acts::RobotnikWinter1,
+            "rmRWZ_Awa" | "rmRWZ2" => Acts::RobotnikWinter2,
+            "rmTPZ1" => Acts::TidalPlant1,
+            "rmTPZ2" => Acts::TidalPlant2,
+            "rmTPZ3" => Acts::TidalPlant3,
+            "rmADZ1" => Acts::AtomicDestroyer1,
+            "rmADZ2" => Acts::AtomicDestroyer2,
+            "rmADZ3" => Acts::AtomicDestroyer3,
+            "rmFinal" => Acts::FinalTrouble,
+            "rmPPZ" => Acts::PurplePalace,
+            "rmFinalEnding" | "rmKnuxEnding" | "rmKnuxEnding2" | "rmCredits" => Acts::Credits,
+            _ => match &self.watchers.levelid.pair {
+                    Some(x) => x.current,
+                    _ => Acts::None,
+                },
+        };
+
+        let state = match room_name_str {
+            "rmDataSelect" => GameState::DataSelect,
+            "rmGameStart" => GameState::GameStart,
+            _ => GameState::Other,
+        };
+
+        self.watchers.state.update(Some(state));
+        self.watchers.levelid.update(Some(act));
     }
 
     fn start(&mut self) -> bool {
@@ -221,7 +282,7 @@ impl State {
         if !settings.start { return false }
         
         let Some(state) = &self.watchers.state.pair else { return false };
-        state.old == 10 && state.current == 12
+        state.old == GameState::DataSelect && state.current == GameState::GameStart
     }
 
     fn split(&mut self) -> bool {
@@ -257,7 +318,7 @@ impl State {
         if !settings.reset { return false }
         
         let Some(state) = &self.watchers.state.pair else { return false };
-        state.old == 10 && state.current == 12
+        state.old == GameState::DataSelect && state.current == GameState::GameStart
     }
 
     fn is_loading(&mut self) -> Option<bool> {
@@ -346,3 +407,16 @@ enum Acts {
     Credits,
     None,
 }
+
+#[derive(Clone, Copy, PartialEq)]
+enum GameState
+{
+    DataSelect,
+    GameStart,
+    Other,
+}
+
+const ROOMID_SIG32: Signature<11> = Signature::new("8B 0D ???????? 83 C4 04 3B 0D");
+const ROOMID_SIG64: Signature<6> = Signature::new("4D 0F 45 F5 8B 0D");
+const ROOMARRAY_SIG32: Signature<8> = Signature::new("8B 3D ???????? 2B EF");
+const ROOMARRAY_SIG64: Signature<13> = Signature::new("74 0C 48 8B 05 ???????? 48 8B 04 D0");
